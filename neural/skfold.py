321b5dd556dc4ea899055f1f3528deb1b99868d4
Emanuele Ballarin (ehteqx) <ehteqx@gmail.com>
2016-12-03 19:01:00 +0100

    Almost-first commit (workspace fine tuning)

---
+++ neural/skfold.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# SKFOLD - Self-organizing Kohonen principal-mainFOLD-approximation map
+# Copyright (c) 2016 Emanuele Ballarin
+# Software released under the terms of the MIT License
+
